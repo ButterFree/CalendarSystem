539b65d4767a4b064d18894e5a5f30ee78f56e8a
Claus L. Henriksen <clih@itu.dk>
2012-10-25 17:09:51 +0200

    added some code

--- CalendarSystem/Controller/Controller.cs
+++ CalendarSystem/Controller/Controller.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CalendarSystem.Model;
 
 namespace CalendarSystem {
   class Controller {
+    private MainView mainView = new MainView();
+    private List<CalendarService> calendarService = new List<CalendarService>();
+
+    public Controller(){
+       mainView.Show();
+    }
   }
 }
